c38e106a4a3796d74b3a3fc9f8dbb0019dd0a72c
Yu <yulliepcruz@gmail.com>
2022-09-24 19:04:41 -0300

    add links

--- script.js
+++ script.js
@@ -4,59 +4,23 @@ const dice2 = document.querySelector(".dice__img-2");
 const result = document.querySelector("span");
 
 refresh.addEventListener("click", () => {
-    const randomNumber1 = Math.round(Math.random() * 6);
-    const randomNumber2 = Math.round(Math.random() * 6);
+  const randomNumber1 = Math.round(Math.random() * 5 + 1);
+  const randomNumber2 = Math.round(Math.random() * 5 + 1);
 
-if (randomNumber1 > randomNumber2) {
+  document
+    .querySelectorAll("img")[0]
+    .setAttribute("src", "./assets/img/dice" + randomNumber1 + ".png");
+  document
+    .querySelectorAll("img")[1]
+    .setAttribute("src", "./assets/img/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
     result.innerHTML = "Player 1 Wins!";
-} else if (randomNumber1 < randomNumber2) {
+  } else if (randomNumber1 < randomNumber2) {
     result.innerHTML = "Player 2 Wins!";
-} else if (randomNumber1 === randomNumber2) {
+  } else if (randomNumber1 === randomNumber2) {
     result.innerHTML = "Tied!";
-} else {
+  } else {
     result.innerHTML = "";
-}
-
-    switch (randomNumber1) {
-        case 1:
-            dice1.setAttribute("src", "assets/img/dice1.png");
-            break;
-        case 2:
-            dice1.setAttribute("src", "assets/img/dice2.png");
-            break;
-        case 3:
-            dice1.setAttribute("src", "assets/img/dice3.png");
-            break;
-        case 4:
-            dice1.setAttribute("src", "assets/img/dice4.png");
-            break;
-        case 5:
-            dice1.setAttribute("src", "assets/img/dice5.png");
-            break;
-        case 6:
-            dice1.setAttribute("src", "assets/img/dice6.png");
-            break;
-    }
-
-    switch (randomNumber2) {
-        case 1:
-            dice2.setAttribute("src", "assets/img/dice1.png");
-            break;
-        case 2:
-            dice2.setAttribute("src", "assets/img/dice2.png");
-            break;
-        case 3:
-            dice2.setAttribute("src", "assets/img/dice3.png");
-            break;
-        case 4:
-            dice2.setAttribute("src", "assets/img/dice4.png");
-            break;
-        case 5:
-            dice2.setAttribute("src", "assets/img/dice5.png");
-            break;
-        case 6:
-            dice2.setAttribute("src", "assets/img/dice6.png");
-            break;
-    }
+  }
 });
-
